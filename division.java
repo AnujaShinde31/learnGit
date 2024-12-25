c222cd31b3d5f130604d152ded347147deaebb1e
Anuja <shindeanuja22@gmail.com>
2024-12-25 21:55:38 +0530

    divsion file added

---
+++ division.java
@@ -0,0 +1,10 @@
+class division {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        System.out.println("The addition is " + (a+b));
+    }
+    
+}
